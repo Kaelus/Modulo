d9c02d2ea610819abe9efadb9f24d362c8738029
Ben Kim <beomheyn.kim@gmail.com>
2022-05-12 20:45:22 +0900

    fix path problem in run.sh

--- deploy/redis/run.sh
+++ deploy/redis/run.sh
@@ -4,4 +4,4 @@ cd ../..
 
 echo Option given = $@
 
-java -jar /home/ben/project/drmbt/drmbt-code/build/libs/drmbt-code-all-1.0.jar -c /home/ben/project/drmbt/drmbt-code/deploy/redis/strata.config "$@"
+java -jar /home/modulo/modulo/build/libs/modulo-all-1.0.jar -c /home/modulo/modulo/deploy/redis/strata.config "$@"
