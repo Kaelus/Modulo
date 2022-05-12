db23182c9019e01e8b3ac468c269e2a6e29ca488
Ben Kim <beomheyn.kim@gmail.com>
2022-05-12 22:02:40 +0900

    fix run.sh

--- deploy/redis/run.sh
+++ deploy/redis/run.sh
@@ -4,4 +4,4 @@ cd ../..
 
 echo Option given = $@
 
-java -jar /home/modulo/modulo/build/libs/drmbt-code-all-1.0.jar -c /home/modulo/modulo/deploy/redis/strata.config "$@"
+java -jar /home/modulo/modulo/build/libs/modulo-all-1.0.jar -c /home/modulo/modulo/deploy/redis/strata.config "$@"
